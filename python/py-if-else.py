68a2745ca38cea8e6825ab518d15b7455fe6a349
sylvia <sylviachadha@gmail.com>
2021-02-04 17:45:35 +0700

    if else

---
+++ python/py-if-else.py
@@ -0,0 +1,19 @@
+# Decision Making
+# Conditional statements
+
+# Indentation makes python recognise if and else
+salary = 6000
+if salary > 5000:
+    print("Approved")
+else:
+    print("Rejected")
+
+# Boolean variables to set flag
+varT = True
+varF = False
+
+if varT:
+    print("The value is true")
+else:
+    print("The value is false")
+
