6d0cf89f77bca1cf44e7580ee5011c5156b0ce91
sylvia <sylviachadha@gmail.com>
2021-02-04 17:22:33 +0700

    welcome python

---
+++ py-welcome.py
@@ -0,0 +1,58 @@
+# My first python program
+
+print("Hello World!!")
+
+# Print variable value
+
+a = "Hello Python"
+print(a)
+
+# Variable (Reserved memory location or space to store some values) types in Python
+# 6 data types
+# 1. Numbers - Integers, Float
+# 2. String - set of characters/words can be accessed by using index ; concatenation
+# 3. Boolean - True / False
+# 4. List
+# 5. Tuple
+# 6. Dictionary
+
+# 1. Numbers
+var_number = 10
+print(var_number)
+
+# 2. String
+var_name = "Sylvia"
+print(var_name)
+# S y l v i a
+# 0 1 2 3 4 5
+print(var_name[1])
+# upper limit index not included
+print(var_name[0:4])
+# concatenation
+var1 = "Machine"
+var2 = "Learning"
+var3 = var1 + var2
+print(var3)
+
+# 3. Boolean
+# use in conditional statements and loops
+varT = True
+varF = False
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
